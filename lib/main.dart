3a45a88b498235e53c7591b37a8871e06a8bc20b
syahli kurniawan <syahlikurniawan332@gmail.com>
2023-10-24 11:28:13 +0700

    ini adalah home

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,19 @@
 import 'package:flutter/material.dart';
+// import 'package:pengelola_keuangan/pages/main_page.dart';
+import 'package:project_manajemen_uang/pages/home/main_page.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  List<Widget> generateBoxes(int n, double w, double h) {
-    List<Widget> boxes = [];
-    List<Color> fillColors = [
-      Colors.blue,
-      Colors.green,
-      Colors.purple,
-      Colors.pink
-    ];
-
-    for (int i = 0; i < n; i++) {
-      Container box = Container(
-        color: fillColors[i],
-        width: w,
-        height: h,
-        margin: const EdgeInsets.all(8.0),
-        child: Center(child: Text(i.toString())),
-      );
-      boxes.add(box);
-    }
-
-    return boxes;
-  }
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter UI Succinctly'),
-        ),
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topRight,
-              end: Alignment.bottomLeft,
-              colors: [Colors.blue, Colors.orange],
-            ),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: generateBoxes(4, 50, 50),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.ac_unit),
-          onPressed: () {
-            print('Oh, it is cold outside...');
-          },
-        ),
-      ),
-      theme: ThemeData(
-        primaryColor: Colors.indigo,
-        hintColor: Colors.amber,
-        textTheme: const TextTheme(
-          bodyMedium: TextStyle(fontSize: 26, fontStyle: FontStyle.italic),
-        ),
-        brightness: Brightness.dark,
-      ),
+      home: MainPage(),
+      theme: ThemeData(primarySwatch: Colors.green),
     );
   }
 }
